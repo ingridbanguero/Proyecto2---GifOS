321dc8cb731a4b51a23fc5a076fbaeb1863a4282
Ingrid Banguero <ingrid_banguero@hotmail.com>
2020-03-11 21:43:29 -0500

    Ya quedo lista la seccion de hoy te sugerimos, funciona el boton de ver mas, falta que funcione la X, tambien ya puse los tags, sacandolos del titulo

--- js/main.js
+++ js/main.js
@@ -44,18 +44,24 @@ function search(q){
         return res.json() // Promise
     }).then(function(json){
         // console.log(json.data[0].images.fixed_width.url)
-        
         let resultsHTML = ''
         json.data.forEach(function(obj){
-            console.log(obj)
+            // console.log(obj)
             // console.log(obj.images.fixed_width.url)
             const url = obj.images.fixed_width.url
-            const width = obj.images.fixed_width.width
-            const height = obj.images.fixed_width.height
             const title = obj.title
-            resultsHTML += `<div class="tendencia"> <img src="${url}" width="${width} alt="${title}" height="${height}"><p>#hashtag</p></div>`
+            let tag = '';
+            // console.log(title);
+            let arrayTitle = title.split(" ");
+            arrayTitle.forEach((elemento)=>{
+                if (elemento !== "GIF"){
+                    tag += '#' + elemento + " "
+                }
+            } );
+            // console.log(arrayTitle);
+            // console.log(tag);
+            resultsHTML += `<div class="tendencia"> <img src="${url}" alt="${title}"><p>${tag}</p></div>`
         })
-
         resultsEl.innerHTML = resultsHTML
     }).catch(function(err){
         console.log(err.message)
@@ -65,7 +71,8 @@ function search(q){
 // Menu sugerencia de resultados de busqueda
 
 const menu_busqueda = document.querySelector('#busqueda');
-searchInput.addEventListener("click", ()=>{
+
+searchInput.addEventListener("click",()=>{
     menu_busqueda.classList.add('active');
 })
 
@@ -87,4 +94,60 @@ sugerencia3.addEventListener('click', ()=>{
     q = sugerencia3.value;
     search(q);
 })
+
+// Gifs de seccion Hoy te sugerimos
+const sugerencias = document.getElementById('sugerencias');
+let sugeridos = ["Jonathan Van Ness", "Sailor Mercury", "Glitter", "Unicorn and Raibows"];
+let sugerenciaHTML = "";
+let nomSug = [];
+let i = 1;
+
+sugeridos.forEach(element => {
+    // console.log(element);
+    const apiKey = '60j6blu7K1BahTceUDM7FC8TRZ6QwbkF';
+    const path = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${element}&limit=12`
+    fetch(path).then(function(res) {
+        return res.json() // Promise
+    }).then(function(json){
+        // console.log(json.data[0].images.fixed_width.url)
+        const url = json.data[0].images.fixed_width.url
+        const title = element.title;
+        
+        nomSug.push(element);
+        sugerenciaHTML += `<div id="sugerencia${i}" class="sugerencia">
+        <div class="titulo_sugerencia">
+            <h3>#${element}</h3>
+            <button onclick="eliminarSug${i}"><img src="./Images/button3.svg" alt=""></button>
+        </div>
+        <div id="sug1" class="imagen_sugerencia">
+            <img src="${url}" alt="${title}">
+            <button href="#seccion_tendencias" onclick="verMas${i}()" value="${element}">Ver más...</a>
+        </div>
+        </div>`
+        
+        // console.log(sugerenciaHTML) 
+        sugerencias.innerHTML = sugerenciaHTML;
+        i++;
+    }).catch(function(err){
+        console.log(err.message)
+    })
+})
+function verMas1(){
+    search(nomSug[0]); 
+}
+function verMas2(){
+    search(nomSug[1]); 
+}
+function verMas3(){
+    search(nomSug[2]); 
+}
+function verMas4(){
+    search(nomSug[3]); 
+}
+
+
+const eliminado = document.getElementById('sugerencia1');
+function eliminarSug1(){
+    console.log('A eliminar')
+}
 
