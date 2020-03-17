f45b22f63e4c94b1805f693f7ba00decb88528b7
Ingrid Banguero <ingrid_banguero@hotmail.com>
2020-03-16 22:42:50 -0500

    Se esta trabajando en la pagina para crear guifos, se logro acceder a la camara y realizar la maquitacion de la primera pagina, es necesario continuar con la maquetacion de la segunda, en donde esta la camara

---
+++ js/crearguifos.js
@@ -0,0 +1,61 @@
+// Evento active del menu para seleccionar el tema
+const btnTema = document.querySelector('#tema');
+const subTemas = document.querySelector('#subtemas');
+
+
+btnTema.addEventListener('click', (e)=> {
+    subTemas.classList.toggle('active');
+    e.stopPropagation();
+})
+
+// Cambio de tema
+const btnDay = document.querySelector('#day');
+const btnNight = document.querySelector('#night');
+const estilo = document.querySelector('#estilo');
+const logo = document.querySelector('#logo');
+
+btnNight.addEventListener('click', ()=>{
+    estilo.setAttribute("href", "./Style/style-night.css");
+    logo.setAttribute("src", "./Images/gifOF_logo_dark.png");
+    localStorage.setItem("dark-modo", "true");
+})
+
+btnDay.addEventListener('click', ()=>{
+    estilo.setAttribute("href", "./Style/style-day.css");
+    logo.setAttribute("src","./Images/gifOF_logo.png");
+    localStorage.setItem("dark-modo", "false");
+})
+
+if(localStorage.getItem("dark-modo") === "true"){
+    estilo.setAttribute("href", "./Style/style-night.css");
+    logo.setAttribute("src", "./Images/gifOF_logo_dark.png");
+} else {
+    estilo.setAttribute("href", "./Style/style-day.css");
+    logo.setAttribute("src","./Images/gifOF_logo.png");
+}
+
+// Cerrar menu de sugerencias y de temas
+window.addEventListener('click', ()=>{
+    subTemas.classList.remove('active');
+})
+
+// Seccion 2 comenzar
+const vista1 = document.getElementById('vista1');
+const btnComenzar = document.getElementById('btnComenzar');
+
+btnComenzar.addEventListener('click', ()=>{
+    vista1.style.display="none";
+})
+
+// Grabacion de video 
+let video = document.querySelector("#video");
+
+document.querySelector("#grabar").addEventListener("click", function(ev){
+    navigator.mediaDevices.getUserMedia({video: true})
+    .then(record)
+    .catch(error => console.log(error));
+})
+
+function record(stream){
+    video.srcObject = stream;
+}
