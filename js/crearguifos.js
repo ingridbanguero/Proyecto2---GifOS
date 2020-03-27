9671794b0a390827fd8eb46d687bcbc17ee9c8a1
Ingrid Banguero <ingrid_banguero@hotmail.com>
2020-03-27 13:55:55 -0500

    Se separo en un archivo aparte todo lo referente a los temas, se soluciono el problema de la grabacion de los gifs al realizar la repeticion

--- js/crearguifos.js
+++ js/crearguifos.js
@@ -1,43 +1,3 @@
-// Evento active del menu para seleccionar el tema
-const btnTema = document.querySelector('#tema');
-const subTemas = document.querySelector('#subtemas');
-
-btnTema.addEventListener('click', (e)=> {
-    subTemas.classList.toggle('active');
-    e.stopPropagation();
-})
-
-// Cambio de tema
-const btnDay = document.querySelector('#day');
-const btnNight = document.querySelector('#night');
-const estilo = document.querySelector('#estilo');
-const logo = document.querySelector('#logo');
-
-btnNight.addEventListener('click', ()=>{
-    estilo.setAttribute("href", "./Style/style-night.css");
-    logo.setAttribute("src", "./Images/gifOF_logo_dark.png");
-    localStorage.setItem("dark-modo", "true");
-})
-
-btnDay.addEventListener('click', ()=>{
-    estilo.setAttribute("href", "./Style/style-day.css");
-    logo.setAttribute("src","./Images/gifOF_logo.png");
-    localStorage.setItem("dark-modo", "false");
-})
-
-if(localStorage.getItem("dark-modo") === "true"){
-    estilo.setAttribute("href", "./Style/style-night.css");
-    logo.setAttribute("src", "./Images/gifOF_logo_dark.png");
-} else {
-    estilo.setAttribute("href", "./Style/style-day.css");
-    logo.setAttribute("src","./Images/gifOF_logo.png");
-}
-
-// Cerrar menu de sugerencias y de temas
-window.addEventListener('click', ()=>{
-    subTemas.classList.remove('active');
-})
-
 // Cambio de vistas
 const vista1 = document.getElementById('vista1');
 const vista2 = document.getElementById('vista2');
@@ -46,27 +6,23 @@ const vista4 = document.getElementById('vista4');
 const opciones1 = document.getElementById('opciones1');
 const opciones2 = document.getElementById('opciones2');
 const opciones3 = document.getElementById('opciones3');
+
+// Botones
 const btnComenzar = document.getElementById('btnComenzar');
 const btnCapturar = document.getElementById('btnCapturar');
 const btnListo = document.getElementById('btnListo');
 const btnRepetir = document.getElementById('btnRepetir');
 const btnSubir = document.getElementById('btnSubir');
 const btnDescargar = document.getElementById('btnDescargar')
-const newGif = document.getElementById('nuevoGif');
 
-btnComenzar.addEventListener('click', ()=>{
-    vista1.style.display="none";
-    vista2.style.display="block";
-    getStreamAndRecord();
-})
-
-
-
+// Elementos para grabar y contener el gif
 const image = document.getElementById('imgGif');
 const video = document.querySelector('video');
-let gifsEl = document.getElementById('misgifs');
+const newGif = document.getElementById('nuevoGif');
+const gifsEl = document.getElementById('misgifs');
 
 
+//Recuperación de "Mis Guifos" guardados en el localStorage
 let gifsGuardados = localStorage.getItem('gif');
 let misGifs = [];
 misGifs = JSON.parse(gifsGuardados);
@@ -79,109 +35,113 @@ if(misGifs !== null){
     misGifs = [];
 }
 
-// Grabacion de video 
-
-function getStreamAndRecord(){
+let recorder; 
+let blob; 
+let urlgif;
+// Eventos 
+btnComenzar.addEventListener('click', (e)=>{
+    vista1.style.display="none";
+    vista2.style.display="block";
     video.style.display="block";
     image.style.display="none";
+    //Promesa
     navigator.mediaDevices.getUserMedia({
         video: true
     }).then(function(stream) {
         video.srcObject = stream;
         video.play();
-        let recorder = RecordRTC(stream, {
+        recorder = RecordRTC(stream, {
             type: 'gif',
             framRate: 1,
             quality: 10,
             hidden: 240,
         });
-        let blob;
-        let urlgif;
         console.log(recorder);
-        btnCapturar.addEventListener('click', (e)=>{
-            if(e.target.id !== "btnCapturar"){
-                console.log('Presionaste un boton y se empezara a grabar'); 
-                opciones1.style.display="none";
-                opciones2.style.display="flex";
-                recorder.startRecording();
-                recorder.stream = stream;
-                btnListo.disabled = false;
-            }
+    })
+}); 
+
+btnCapturar.addEventListener('click', (e)=>{
+    if(e.target.id !== "btnCapturar"){
+        console.log('Presionaste un boton y se empezara a grabar'); 
+        opciones1.style.display="none";
+        opciones2.style.display="flex";
+        recorder.startRecording();
+/*         recorder.stream = stream; */
+    }
+})
+
+btnListo.addEventListener('click', (e)=>{
+    if(e.target.id !== "btnListo"){
+        console.log('Presionaste un boton y se va a guardar lo que grabaste'); 
+        opciones2.style.display="none";
+        opciones3.style.display="flex";
+        recorder.stopRecording(function() {
+            video.style.display="none";
+            image.style.display="block";
+            blob = recorder.getBlob();
+            image.src = URL.createObjectURL(blob);
+            console.log(blob);
+            console.log(recorder);
             
         })
-        
-        btnListo.addEventListener('click', (e)=>{
-            if(e.target.id !== "btnListo"){
-                console.log('Presionaste un boton y se va a guardar lo que grabaste'); 
-                opciones2.style.display="none";
-                opciones3.style.display="flex";
-                this.disabled = true;
-                recorder.stopRecording(function() {
-                    video.style.display="none";
-                    image.style.display="block";
-                    blob = recorder.getBlob();
-                    image.src = URL.createObjectURL(blob);
-                    console.log(blob);
-                    console.log(recorder);
-                    
-                })
-            }
-        });
+    }
+});
 
-        btnSubir.addEventListener('click', (e) =>{
-            vista2.style.display="none";
-            vista3.style.display="block";
-            let form = new FormData();
-            form.append('file', blob, 'GifRecorder.gif')
-            console.log(form.get('file'))
+btnRepetir.addEventListener('click', (e)=>{
+    opciones3.style.display="none";
+    opciones1.style.display="block";
+    recorder.reset();
+    video.style.display="block";
+    image.style.display="none";
+    video.play();
+})
 
-            const api_key = '60j6blu7K1BahTceUDM7FC8TRZ6QwbkF'
-            const url = `http://upload.giphy.com/v1/gifs`
+btnSubir.addEventListener('click', (e) =>{
+    vista2.style.display="none";
+    vista3.style.display="block";
+    let form = new FormData();
+    form.append('file', blob, 'GifRecorder.gif')
+    console.log(form.get('file'))
+    const api_key = '60j6blu7K1BahTceUDM7FC8TRZ6QwbkF'
+    const url = `http://upload.giphy.com/v1/gifs`
+    form.append('api_key', api_key)
 
-            form.append('api_key', api_key)
-
-            fetch(url,{
-                method:'POST',
-                body: form,
-            }).then(res=>res.json())
-            .then(datar=>{
-                console.log(datar.data.id)
-                let idgif = datar.data.id;
-                console.log(datar);
-                urlgif = `http://api.giphy.com/v1/gifs/${idgif}?api_key=${api_key}`
-                console.log(urlgif);
-                fetch(urlgif).then(res => res.json())
-                    .then(json =>{
-                    console.log(json);
-                    console.log(json.data.images.fixed_width.url);
-                    let urlImage = json.data.images.fixed_width.url;
-                    newGif.setAttribute('src', urlImage);
-                    vista3.style.display="none";
-                    vista4.style.display="block";
-                    añadirGif(urlImage);
-                })
-            })
-        });
-        btnDescargar.addEventListener('click', ()=>{
-            recorder.save();
+    fetch(url,{
+        method:'POST',
+        body: form,
+    }).then(res=>res.json())
+    .then(datar=>{
+        console.log(datar.data.id)
+        let idgif = datar.data.id;
+        console.log(datar);
+        urlgif = `http://api.giphy.com/v1/gifs/${idgif}?api_key=${api_key}`
+        console.log(urlgif);
+        fetch(urlgif).then(res => res.json())
+            .then(json =>{
+            console.log(json);
+            console.log(json.data.images.fixed_width.url);
+            let urlImage = json.data.images.fixed_width.url;
+            newGif.setAttribute('src', urlImage);
+            vista3.style.display="none";
+            vista4.style.display="block";
+            añadirGif(urlImage);
+            
         })
-        btnRepetir.addEventListener('click', ()=>{
-            opciones3.style.display="none";
-            opciones1.style.display="block";
-            recorder.stream.stop()
-            recorder.destroy();
-            recorder = null;
-            getStreamAndRecord();
-        })
-        
-    }); 
-} 
+    })
+});
+
+btnDescargar.addEventListener('click', ()=>{
+    recorder.save();
+})
+
+// LA FUNCION NO ESTA TERMINANDO Y YO ESTOY INICIANDO OTRA Y OTRA CADA VEZ QUE LA REPITO
+
+
+// Grabacion de video 
 
 function añadirGif(urlImage){
     misGifs.push(urlImage);
-    misGifs.forEach(element =>{
-        gifHTML = `<div class="misGuifos"><img src="${element}" alt="Gif subido"></div>`
-        gifsEl.innerHTML += gifHTML;
-    })
+    gifHTML = `<div class="misGuifos"><img src="${urlImage}" alt="Gif subido"></div>`
+    gifsEl.innerHTML += gifHTML;
     localStorage.setItem('gif', JSON.stringify(misGifs));
 }
--- js/main.js
+++ js/main.js
@@ -1,39 +1,3 @@
-
-// Evento active del menu para seleccionar el tema
-const btnTema = document.querySelector('#tema');
-const subTemas = document.querySelector('#subtemas');
-
-
-btnTema.addEventListener('click', (e)=> {
-    subTemas.classList.toggle('active');
-    e.stopPropagation();
-})
-
-// Cambio de tema
-const btnDay = document.querySelector('#day');
-const btnNight = document.querySelector('#night');
-const estilo = document.querySelector('#estilo');
-const logo = document.querySelector('#logo');
-
-btnNight.addEventListener('click', ()=>{
-    estilo.setAttribute("href", "./Style/style-night.css");
-    logo.setAttribute("src", "./Images/gifOF_logo_dark.png");
-    localStorage.setItem("dark-modo", "true");
-})
-
-btnDay.addEventListener('click', ()=>{
-    estilo.setAttribute("href", "./Style/style-day.css");
-    logo.setAttribute("src","./Images/gifOF_logo.png");
-    localStorage.setItem("dark-modo", "false");
-})
-
-if(localStorage.getItem("dark-modo") === "true"){
-    estilo.setAttribute("href", "./Style/style-night.css");
-    logo.setAttribute("src", "./Images/gifOF_logo_dark.png");
-} else {
-    estilo.setAttribute("href", "./Style/style-day.css");
-    logo.setAttribute("src","./Images/gifOF_logo.png");
-}
 // Recuperacion datos del historial de sugerencia de busqueda
 
 
@@ -173,8 +137,7 @@ sug.addEventListener('click', (e)=>{
         padre.parentNode.remove(padre);
     }
 })
-// Cerrar menu de sugerencias y de temas
+// Cerrar menu de sugerencias
 window.addEventListener('click', ()=>{
     menu_busqueda.classList.remove('active');
-    subTemas.classList.remove('active');
 })
